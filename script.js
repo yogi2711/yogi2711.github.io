627c266400bc338bed02675b03c150be62cc8b7e
Yogiprastyo <Yogiprasetyo242@gmail.com>
2025-08-14 10:15:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function revealOnScroll() {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 100; // jarak sebelum elemen muncul
+
+        if (elementTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealOnScroll);
